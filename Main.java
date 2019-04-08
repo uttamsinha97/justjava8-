0675bf9b52baa6fcb627fa3155e7f98c801d8b00
uttamSinha <uttamkumar.sinha95@gmail.com>
2019-04-08 11:22:10 +0530

    For calculating duration

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
+import java.time.Instant;
+import java.time.*;
+import java.time.temporal.ChronoUnit;  
+public class Main
+{
+  public static void main(String[] args) throws ParseException
+  {
+    Instant start = Instant.now();
+
+    //Measure execution time for this method
+    methodToTime();
+
+    Instant finish = Instant.now();
+
+    long timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+  }
+
+  private static void methodToTime() {
+    try {
+      TimeUnit.SECONDS.sleep(3);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
